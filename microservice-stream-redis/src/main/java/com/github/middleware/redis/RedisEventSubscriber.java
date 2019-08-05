8aa66c8ce9e8e7073392e3a976ec721d9ffe7236
zhangchao <jaguar_zc@sina.com>
2019-08-05 16:12:04 +0800

    1

---
+++ microservice-stream-redis/src/main/java/com/github/middleware/redis/RedisEventSubscriber.java
@@ -0,0 +1,85 @@
+package com.github.middleware.redis;
+
+import com.github.middleware.event.EventSubscriber;
+import com.github.middleware.message.MessageData;
+import com.github.middleware.message.MessageType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPubSub;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import static com.github.middleware.utils.GsonUtils.toObject;
+
+
+/**
+ * @author: zhangchao
+ * @time: 2018-12-25 10:42
+ **/
+public class RedisEventSubscriber extends EventSubscriber {
+    public final static Logger log = LoggerFactory.getLogger(RedisEventSubscriber.class);
+
+    private RedisChannelProvider redisChannelProvider;
+
+
+    private boolean running;
+
+    public RedisEventSubscriber(RedisChannelProvider redisChannelProvider) {
+        super();
+        this.redisChannelProvider = redisChannelProvider;
+    }
+
+    public void start(MessageType type) {
+        running = true;
+        if(type == MessageType.P2M){
+            new Thread(new Runnable() {
+                public void run() {
+                    RedisCmd redisTemplate = redisChannelProvider.getRedisCmd();
+                    redisTemplate.execute(new RedisCmd.Cmd() {
+                        public void cmd(Jedis jedis) {
+                            jedis.subscribe(new JedisPubSub() {
+                                @Override
+                                public void onMessage(String channel, String message) {
+                                    log.info("subscribe:{}:{}",channel,message);
+                                    MessageData messageData = (MessageData)toObject(message, MessageData.class);
+                                    Object data = toObject(String.valueOf(messageData.getData()), getEventDataObjectClass());
+                                    eventHandler.handler((Serializable) data);
+                                }
+                            },getEventHandler().getEventName());
+                        }
+                    });
+                }
+            }).start();
+        }else if(type == MessageType.P2P){
+        new Thread(new Runnable() {
+            public void run() {
+                RedisCmd redisCmd = redisChannelProvider.getRedisCmd();
+                while (running){
+                    try {
+                        redisCmd.execute(new RedisCmd.Cmd() {
+                            public void cmd(Jedis jedis) {
+                                Optional<String> optional = Optional.ofNullable(jedis.rpop(getEventHandler().getEventName()));
+                                if(optional.isPresent()){
+                                    MessageData messageData = (MessageData)toObject(optional.get(), MessageData.class);
+                                    Object data = toObject(String.valueOf(messageData.getData()), getEventDataObjectClass());
+                                    eventHandler.handler((Serializable) data);
+                                }
+                            }
+                        });
+                        Thread.sleep(200);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
+}
---
+++ microservice-stream-redis/src/test/java/com/github/middleware/redis/RedisChannelProviderTest.java
@@ -0,0 +1,81 @@
+package com.github.middleware.redis;
+
+import com.github.middleware.Stream;
+import com.github.middleware.channel.ChannelProvider;
+import com.github.middleware.config.EventConfigItem;
+import com.github.middleware.config.EventConfigManager;
+import com.github.middleware.event.EventHandler;
+
+import java.util.Properties;
+import java.util.Scanner;
+
+public class RedisChannelProviderTest {
+
+
+    public static void main(String[] args) {
+
+
+        Properties properties = new Properties();
+        properties.setProperty("host","localhost:6379");
+        RedisChannelConfig config = new RedisChannelConfig(properties);
+
+        ChannelProvider channelProvider = new RedisChannelProvider(config);
+
+
+        EventConfigItem a = new EventConfigItem();
+        a.setChannelProvider(channelProvider);
+        a.setEventName(A.EVENT_NAME);
+        EventConfigManager.addEventSubcriberConfigItem(a);
+
+        EventConfigItem b = new EventConfigItem();
+        b.setChannelProvider(channelProvider);
+        b.setEventName(B.EVENT_NAME);
+        EventConfigManager.addEventSubcriberConfigItem(b);
+
+        channelProvider.init();
+
+        Stream.register(new  A());
+        Stream.register(new  A());
+        Stream.register(new  A());
+        Stream.register(new  B());
+        Stream.register(new  B());
+        Stream.register(new  B());
+
+        String eventName = "";
+        Scanner scanner  = new Scanner(System.in);
+        while (!(eventName = scanner.next()).equals("exit")){
+            try {
+                String[] split = eventName.split(":");
+                Stream.publish(split[0], split[1]);
+            }catch (Exception e){
+
+            }
+        }
+    }
+}
+
+
+class A implements EventHandler<String> {
+    public static String EVENT_NAME = "A";
+    public void handler(String s) {
+        System.out.println(EVENT_NAME+"收到消息:"+s);
+    }
+
+    public String getEventName() {
+        return EVENT_NAME;
+    }
+}
+
+
+
+
+class B implements EventHandler<String>{
+    public static String EVENT_NAME = "B";
+    public void handler(String s) {
+        System.out.println(EVENT_NAME+"收到消息:"+s);
+    }
+
+    public String getEventName() {
+        return EVENT_NAME;
+    }
+}
